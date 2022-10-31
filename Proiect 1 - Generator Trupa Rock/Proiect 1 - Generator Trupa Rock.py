bc8da9fa734a08a732daf2cb97bded948872348b
Daniel Conț <114297966+DanielCont2001@users.noreply.github.com>
2022-10-31 17:56:49 +0200

    Delete Proiect 1 - Generator Trupa Rock directory

--- Proiect 1 - Generator Trupa Rock/Proiect 1 - Generator Trupa Rock.py
+++
@@ -1,4 +0,0 @@
-print("Bine ai venit la generatorul de nume pentru trupe Rock!\n")
-nume_oras = input("Care este numele orasului unde te-ai nascut?\n")
-nume_animal = input("Care este numele primului animal de companie?\n")
-print("Numele trupei tale de Rock este: " + nume_oras + " " + nume_animal)
